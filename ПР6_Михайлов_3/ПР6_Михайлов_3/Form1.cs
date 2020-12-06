3c89367ffafc94411f97a66eecb299bfb964f95d
mrProger <60040129+mrProger@users.noreply.github.com>
2020-12-06 22:30:36 +0600

    Add files via upload

---
+++ ПР6_Михайлов_3/ПР6_Михайлов_3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПР6_Михайлов_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double h = double.Parse(textBox3.Text);
+
+            textBox4.Text = Convert.ToString(((a + b) / 2) * h);
+        }
+    }
+}
