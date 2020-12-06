d9a3e848b555c9ab017acd7f97e6732ea39d95de
mrProger <60040129+mrProger@users.noreply.github.com>
2020-12-06 21:43:58 +0600

    Add files via upload

---
+++ ПР7_Михайлов/ПР7_Михайлов/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПР7_Михайлов
+{
+    public partial class Form1 : Form
+    {
+        public double x = 14.26, y = -1.22, z = 3.5 * Math.Pow(10, -2), t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = (((2 * Math.Cos(x - Math.PI / 6)) / (0.5 + (Math.Sin(y) * Math.Sin(y))) * (1 + (z*z) / (3 - z*z / 5))));
+            label1.Text = t.ToString();
+        }
+    }
+}
